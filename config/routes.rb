99f25444229455c1629aebd88a39c73bf233525b
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2023-01-15 00:11:09 +0430

    greate home page and set as root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :groups
-  resources :entities
+  devise_for :users
+  root "splash#index"
+  resources :groups do
+    resources :entities, only: [:new, :create, :edit, :update]
+  end
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
