76bc831eea07a9b4f4def51596b8a485aa000cf5
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2023-01-14 23:56:54 +0430

    update group  controller and setup devis on

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,31 +1,32 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: %i[ show edit update destroy ]
+  before_action :set_group, only: %i[show edit update destroy]
+  before_action :set_user
 
   # GET /groups or /groups.json
   def index
-    @groups = Group.all
+    @groups = @user.groups
   end
 
   # GET /groups/1 or /groups/1.json
   def show
+    @entities = @group.entities.order(created_at: :desc)
   end
 
   # GET /groups/new
   def new
-    @group = Group.new
+    @group = @user.groups.build
   end
 
   # GET /groups/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /groups or /groups.json
   def create
-    @group = Group.new(group_params)
+    @group = @user.groups.build(group_params)
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to group_url(@group), notice: "Group was successfully created." }
+        format.html { redirect_to groups_path, notice: 'Group was successfully created.' }
         format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +39,7 @@ class GroupsController < ApplicationController
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: "Group was successfully updated." }
+        format.html { redirect_to group_url(@group), notice: 'Group was successfully updated.' }
         format.json { render :show, status: :ok, location: @group }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +53,24 @@ class GroupsController < ApplicationController
     @group.destroy
 
     respond_to do |format|
-      format.html { redirect_to groups_url, notice: "Group was successfully destroyed." }
+      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_group
-      @group = Group.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def group_params
-      params.fetch(:group, {})
-    end
+  def set_user
+    @user = current_user
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
